311b28a5fab897eadeb53b70fbec82731c3c599e
kehr <kehr.china@gmail.com>
2014-04-30 13:03:12 +0800

    调整目录结构

---
+++ upcloud/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# @File Name:    __init__.py
+# @Author:	     kehr
+# @Mail:		 kehr.china@gmail.com
+# @Created Time: Wed 30 Apr 2014 11:01:53 AM CST
+# @Copyright:    MIT applies
+#########################################################################
+
+__version__ = '0.1.0'
+__author__  = 'kehr(wang kaixuan)'
+__mail__    = 'kehr.china@gmail.com'
+__license__ = 'MIT License: http://opensource.org/licenses/MIT'
+__all__ = ['cli','upcloud']    
